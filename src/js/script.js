5d4468ed47cd9d83af534ba01dbaca64e6b1f962
Adrianna Nowak <adriannanowakk@gmail.com>
2023-04-23 10:28:21 +0200

    add render function

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,8 @@
         },
         containerOf: {
             bookList: '.books-list',
+            bookImage: '.books-list .book__image',
+            // bookImageId: '.books-list '
         }
     }
 
@@ -18,23 +20,44 @@
 
         /* find books container */
         const booksContainer = document.querySelector(select.containerOf.bookList);
-        console.log('booksContainer:', booksContainer)
+        // console.log('booksContainer:', booksContainer)
 
-
+        // go through every book in the dataSource.books
         for (let book of dataSource.books) {
-            console.log('book:', book)
+            // console.log('book:', book)
 
-            /* generate HTML based on template */
+            /* generate HTML code based on 'book' template */
             const generatedHTML = templates.book(book);
-            console.log('generatedHTML:', generatedHTML)
+            // console.log('generatedHTML:', generatedHTML)
 
-            /* create element using utils.createElementFromHTML */
+            /* create DOM element using utils.createElementFromHTML */
             const domHtmlElement = utils.createDOMFromHTML(generatedHTML)
 
-            // /* add book to book list */
+            // /* add book (DOM element) to book list as a new child */
             booksContainer.appendChild(domHtmlElement);
         }
     }
 
+    const favouriteBooks = [];
+
+    function initActions() {
+
+
+        const booksImageContainer = document.querySelectorAll(select.containerOf.bookImage);
+
+
+        for (const bookImage of booksImageContainer) {
+
+            bookImage.addEventListener('dblclick', function (event) {
+                event.preventDefault();
+
+                bookImage.classList.add('favorite');
+
+                let bookImageDataId = bookImage.getAttribute('data-id');
+                favouriteBooks.push(bookImageDataId);
+            })
+        }
+    }
     render();
+    initActions();
 }
